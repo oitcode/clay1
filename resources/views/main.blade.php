cea301111c0b6c39424d0e3879e1575e939c0bea
SPS <sps@oit.com.np>
2022-08-12 09:02:08 +0545

    Using layouts app in main file

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,1 +1,23 @@
-THIS IS GONNA BE GOOD
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
+
+        <div class="card-body">
+          @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                  {{ session('status') }}
+              </div>
+          @endif
+
+          THIS IS GONNA BE GOOD
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
