f2b98a3df5bf5b89d679a3861eb95313ac9e38bf
SPS <sps@oit.com.np>
2022-12-05 15:59:19 +0545

    Added why choose us partial

---
+++ resources/views/partials/why-choose-us.blade.php
@@ -0,0 +1,91 @@
+<div class="container-fluid bg-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 py-5 text-white">
+        <span class="border badge badge-pill" style="font-size: 1.3rem;">
+        Features
+        </span>
+
+        <h2 class="h1 mt-3 mb-4" style="font-family: Arial; font-weight: bold;">
+          Why choose us
+        </h2>
+
+        <p class="h5 mb-5">
+          With our experienced engineers you are guaranteed to get valuable
+          consultation. You can rely on the quality of our services and the
+          professional support you get from us.
+        </p>
+
+        <div class="row mb-4">
+          <div class="col-md-6">
+            <div class="d-flex">
+              <div class="d-flex flex-column justify-content-center">
+                <i class="fas fa-check-circle fa-2x mr-3"></i>
+              </div>
+              <div>
+                <div class="h5 mb-2">
+                  Experienced
+                </div>
+                <div class="h4" style="font-weight: bold;">
+                  Engineers
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="d-flex">
+              <div class="d-flex flex-column justify-content-center">
+                <i class="fas fa-check-circle fa-2x mr-3"></i>
+              </div>
+              <div>
+                <div class="h5 mb-2">
+                  Quality
+                </div>
+                <div class="h4" style="font-weight: bold;">
+                  Service
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md-6">
+            <div class="d-flex">
+              <div class="d-flex flex-column justify-content-center">
+                <i class="fas fa-check-circle fa-2x mr-3"></i>
+              </div>
+              <div>
+                <div class="h5 mb-2">
+                  Valuable
+                </div>
+                <div class="h4" style="font-weight: bold;">
+                  Consultation
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="d-flex">
+              <div class="d-flex flex-column justify-content-center">
+                <i class="fas fa-check-circle fa-2x mr-3"></i>
+              </div>
+              <div>
+                <div class="h5 mb-2">
+                  Professional
+                </div>
+                <div class="h4" style="font-weight: bold;">
+                  Support
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <img src="{{ asset('img/bg-image-2.jpg') }}" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</div>
