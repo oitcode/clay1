66fe5c9cb4cb6e8b1b276e09a306ad56b1cadbd5
SPS <sps@oit.com.np>
2022-12-10 12:15:44 +0545

    misc

---
+++ resources/views/partials/services.blade.php
@@ -0,0 +1,168 @@
+{{-- Services para --}}
+<div class="container-fluid mb-5">
+
+  <div class="container text-center">
+    <span class="border badge badge-pill text-secondary" style="font-size: 1.3rem;">
+    Services
+    </span>
+    <h2 class="h1 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+      Civil and Geotechnical Engineering
+    </h2>
+  </div>
+
+
+  <div class="container">
+    <div class="row">
+
+      {{-- Geotechnical investigation --}}
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-mission.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+                Geotechnical investigation
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We perform surface and subsurface exploration
+                of a site including soil testing and much more. 
+              </div>
+              <a href="{{ route('services-geotechnical-investigation') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {{-- Design works --}}
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-vision.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+                Design works
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We do design works for building, bridge and many other types of constructions. 
+              </div>
+              <a href="{{ route('services-design-works') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {{-- Protection works design --}}
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-objective.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+                Protection works design
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We do protection works design and our designs have
+                helped our clients complete their high quaity project.
+              </div>
+              <a href="{{ route('services-protection-works-design') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+
+      {{-- Slop stability analysis --}}
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-mission.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4 text-center" style="color: #004; font-family: Arial; font-weight: bold;">
+                Slope stability analysis
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We evaluate the stability of earth and rock-fill dams,
+                embankments, excavated slopes, and natural slopes in soil and rock.
+              </div>
+              <a href="{{ route('services-slope-stability-analysis') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {{-- Topographical survey --}}
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-vision.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+                Topographical survey
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We also work on recording the coordinates and
+                elevation (height) data of natural and artificial
+                features within a designated survey area. 
+              </div>
+              <a href="{{ route('services-topographical-survey') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {{-- Construction supervision --}}
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <div class="card-body p-0" style="background-color: #d9f1ff;">
+            <img src="{{ asset('img/about-objective.jpg') }}" class="img-fluid w-100">
+            <div class="text-center p-2">
+              <h2 class="h3 mt-3 mb-4" style="color: #004; font-family: Arial; font-weight: bold;">
+                Construction supervision
+              </h2>
+              <div class="h5 text-secondary text-left">
+                We provide supervision of construction works so
+                that the project meets the enigneering and quality standards.
+              </div>
+              <a href="{{ route('services-construction-supervision') }}"
+                  class="btn btn-primary badge-pill mt-3 p-3 px-5">
+                <span class="h5">
+                Read more
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
